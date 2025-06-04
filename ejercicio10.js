a4006f28f43c6b26ef17fda76fca41e59b642d94
aquileorl <ejruizlinares@gmail.com>
2025-06-04 19:09:07 +0200

    Iniciando ej10

---
+++ ejercicio10.js
@@ -0,0 +1,25 @@
+
+const capitals = {
+    Spain: 'Madrid',
+    France: 'Paris',
+    Italy: 'Rome',
+    Germany: 'Berlin',
+    Portugal: 'Lisbon',
+    Poland: 'Warsaw',
+    Greece: 'Athens',
+    Austria: 'Vienna',
+    Hungary: 'Budapest',
+    Ireland: 'Dublin'
+  };
+
+  const keys = Object.keys(capitals);
+  console.log(keys);
+  
+  function getCapital(country) {
+    // insert code
+    let encontrado = false;
+
+    
+  }
+  
+  
